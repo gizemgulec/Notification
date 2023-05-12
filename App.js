7abce96ce1d0350c01f8fa098568af2be145d6c0
Gizem Gulec <gizem.gulec@dominospizza.com.tr>
2023-05-12 14:54:24 +0300

    added App.js

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+export async function allowsNotificationsAsync() {
+  const settings = await Notifications.getPermissionsAsync();
+  return (
+    settings.granted || settings.ios?.status === Notifications.IosAuthorizationStatus.PROVISIONAL
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button onPress={schedulePushNotification} title="Send Notification" />
     </View>
   );
+}
+async function schedulePushNotification() {
+
+  const hasPushNotificationPermissionGranted = await allowsNotificationsAsync()
+
+  if (hasPushNotificationPermissionGranted) {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! ",
+        body: 'Here is the notification body',
+        data: { data: 'goes here' },
+      },
+      trigger: { seconds: 3 },
+    });
+
+  }
+
 }
 
 const styles = StyleSheet.create({
